e664b612db809347b4fede5e46d0c1ccd7b31abe
Johnwook Choi <johnwook.choi@gmail.com>
2015-08-26 13:28:32 +0900

    gulp task 'publish' 작성 시작

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,9 +4,12 @@ import gulpLoadPlugins from 'gulp-load-plugins';
 import browserSync from 'browser-sync';
 import del from 'del';
 import {stream as wiredep} from 'wiredep';
+import {credentials as awsConfig} from './AWSConfig';
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
+
+var s3 = require('gulp-s3-upload')(awsConfig);
 
 gulp.task('styles', () => {
   'use strict';
@@ -183,4 +186,13 @@ gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
 gulp.task('default', ['clean'], () => {
   'use strict';
   gulp.start('build');
+});
+
+gulp.task('publish', () => {
+  'use strict';
+  gulp.src('a.text')
+    .pipe($.s3Upload(awsConfig)({
+      Bucket: 'www.trillionaire.co.kr', //  Required
+      ACL: 'public-read'       //  Needs to be user-defined
+    }));
 });
